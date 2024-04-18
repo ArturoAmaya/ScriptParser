1380c393b28a04eb1e0f83ba2a17aa9b8332b012
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-18 15:27:52 -0700

    edited parse to use scene objects

--- parse.py
+++ parse.py
@@ -1,4 +1,4 @@
-from scriptparser import scene
+from scriptparser import scene, slide_source, style, style_type, transition, transition_in
 
 def parse_header(header:list[str]):
     true_header = dict()
@@ -26,13 +26,49 @@ def parse_header(header:list[str]):
             true_header["Slides"].append(line.strip())
     return true_header
 
-def parse_script(script:list[str]):
+def parse_script(script:list[scene], header:dict):
     true_script = []
+    count = 0
     for line in script:
-        snippets = line.split(r"\\")
-        for snippet in snippets:
-            if snippet != '':
-                true_script.append(snippet)
+        # make a new scene
+        s = scene()
+
+        # assign the number
+        s.number = count
+        
+
+        # assign the text, the background type, the slides url, the style
+        s.text = line.replace('\\\\', '')
+        s.slide.slide_source_type = slide_source.URL
+        s.slide.slide_url = header['Slides'][count]
+
+        # default here is pip so no background to be set
+        # assign style per default for v0.01 using ffmpeg notation
+        s.style.style = style_type.PIP
+        s.style.avatar_scale = 0.5
+        s.style.slides_scale = 1.0
+        s.style.avatar_position = (0.75,0.75)
+        s.style.slides_position = (0.0,0.0)
+        #style(style_type.PIP, 0.5, 1.0, (0.75,0.75), (0.0,0.0))
+
+        # transition infor
+        if (count != 0):
+            s.transition_in.t_type = transition.FADE
+            s.transition_in.duration = 1.0
+        
+        # at this point, the only things in scene with no value are
+        # avatar_video - id, metadata, url and video depend on uploading the clips
+        # this is PIP only so no background needed
+        # caption - same reason as avatar_video
+        # clip - video hasn't been made yet
+        # slide img - havent gotten the slides yet
+        # the first slide should have no transition in information
+
+        # increment the count
+        count = count + 1
+
+        # add the scene to the scene list
+        true_script.append(s)
     return true_script
 
 def parse_from_file(filepath: str):
@@ -49,9 +85,10 @@ def parse_from_file(filepath: str):
             elif line.strip() == "--":
                 head = False
             else:
-                script.append(str(line.strip()))
+                if (str(line.strip())!=''):
+                    script.append(str(line.strip()))
         header = parse_header(header)
-        script = parse_script(script)
+        script = parse_script(script, header)
         return (header, script)
     except:
         return False
