ebeec42914a7fcb1c2dd71796f5694a2c307309a
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-18 15:28:05 -0700

    OOP scaffolding

--- scriptparser/__init__.py
+++ scriptparser/__init__.py
@@ -2,5 +2,6 @@ from .types.avatar_video import avatar_video
 from .types.background import background
 from .types.caption import caption
 from .types.scene import scene
-from .types.slide import slide
-from .types.transition_in import transition_in
+from .types.slide import slide, slide_source
+from .types.transition_in import transition_in, transition
+from .types.style import style, style_type
