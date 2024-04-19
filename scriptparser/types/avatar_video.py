ec667f2d2a7681ef01505298c51fd6bf732fdef6
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-19 15:39:39 -0700

    small OOP edits to track filenames

--- scriptparser/types/avatar_video.py
+++ scriptparser/types/avatar_video.py
@@ -2,13 +2,15 @@ from dataclasses import dataclass
 
 @dataclass
 class avatar_video:
-    id: str=None
-    url: str=None
-    video: object=None
-    metadata: object=None
+    id: str
+    url: str
+    video: object
+    metadata: object
+    filename: str
 
     def __init__(self):
         self.id = None
         self.url = None
         self.video = None
         self.metadata = None
+        self.filename = None
--- scriptparser/types/slide.py
+++ scriptparser/types/slide.py
@@ -8,9 +8,11 @@ class slide_source(Enum):
 class slide:
     slide_source_type: slide_source
     slide_url: str | None
-    slide_img: any
+    slide_img: object
+    slide_filename: str
 
     def __init__(self):
         self.slide_source_type = None
         self.slide_url = None
         self.slide_img = None
+        self.slide_filename = None
