4b44ac8e86896f4e0b134151dc5ed23ee28612b0
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-19 15:41:29 -0700

    starting to use the ffmpeg wrapper, it's weird

--- play.py
+++ play.py
@@ -1,5 +1,11 @@
 import ffmpeg
 
-innie = ffmpeg.input('clip1.mp4')
-probes = ffmpeg.probe('clip1.mp4')
-print(innie)
+innie = ffmpeg.input('vid.mp4')
+probes = ffmpeg.probe('vid.mp4')
+video_stream = next((stream for stream in probes['streams'] if stream['codec_type'] == 'video'), None)
+print(probes)
+outie = ffmpeg.input('vid.mp4')
+
+width = int(video_stream['width'])
+height = int(video_stream['height'])
+innie = innie.filter('scale', width = f"{width/4}", height=f"{height/4}").output('output.mp4').run()
