e2ef948f3bb71f07c97832e532ed251ed4b81d6f
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-22 16:54:56 -0700

    figured out *basic* function for transitioning clips

--- play.py
+++ play.py
@@ -1,11 +1,36 @@
 import ffmpeg
+import transition
+from scriptparser import transition_type
 
-innie = ffmpeg.input('vid.mp4')
-probes = ffmpeg.probe('vid.mp4')
-video_stream = next((stream for stream in probes['streams'] if stream['codec_type'] == 'video'), None)
-print(probes)
-outie = ffmpeg.input('vid.mp4')
+vid1 = ffmpeg.input('avatar0.mp4')
+probe1 = ffmpeg.probe('avatar0.mp4')
 
-width = int(video_stream['width'])
-height = int(video_stream['height'])
-innie = innie.filter('scale', width = f"{width/4}", height=f"{height/4}").output('output.mp4').run()
+vid2 = ffmpeg.input('avatar1.mp4')
+probe2 = ffmpeg.probe('avatar1.mp4')
+#video_stream = next((stream for stream in probes['streams'] if stream['codec_type'] == 'video'), None)
+#print(probes)
+#outie = ffmpeg.input('vid.mp4')
+
+#width = int(video_stream['width'])
+#height = int(video_stream['height'])
+#innie = innie.filter('scale', width = f"{width/4}", height=f"{height/4}").output('output.mp4').run()
+
+# let's try a fade transition
+vid1_vid_stream = next((stream for stream in probe1['streams'] if stream['codec_type'] == 'video'), None)
+vid1_audio_stream = next((stream for stream in probe1['streams'] if stream['codec_type'] == 'audio'), None)
+
+vid2_vid_stream = next((stream for stream in probe2['streams'] if stream['codec_type'] == 'video'), None)
+vid2_audio_stream = next((stream for stream in probe2['streams'] if stream['codec_type'] == 'audio'), None)
+
+
+clip =ffmpeg.filter([vid1,vid2], 'xfade', transition='fade', duration=1, offset=(float(vid1_vid_stream['duration'])-1-(float(vid1_vid_stream['duration'])-float(vid1_audio_stream['duration']))))
+audio = ffmpeg.filter([vid1.audio, vid2.audio], 'acrossfade', duration=1)
+ffmpeg.output(clip,audio,'output.mp4', pix_fmt='yuv420p').run()
+
+
+#vid1 = vid1.filter('xfade', duration=1, offset=(float(vid1_vid_stream['duration'])-1-(float(vid1_vid_stream['duration'])-float(vid1_audio_stream['duration']))))
+
+(v, a, v_d, a_d) = transition.transition(vid1,vid2,vid1.audio,
+                                         vid2.audio,transition_type.FADE,1.0,
+                                         float(vid1_vid_stream['duration']), float(vid2_vid_stream['duration']), float(vid1_audio_stream['duration']), float(vid2_audio_stream['duration']))
+ffmpeg.output(v,a,'output2.mp4',pix_fmt='yuv420p').run()
--- transition.py
+++ transition.py
@@ -1,7 +1,20 @@
-from scriptparser import scene
+from scriptparser import scene, style_type, transition_type
+import ffmpeg
 
-def transition(scene0: scene, scene1: scene):
-    pass
+def transition(v0: ffmpeg.nodes.FilterableStream, v1: ffmpeg.nodes.FilterableStream, a0: ffmpeg.nodes.FilterableStream, a1: ffmpeg.nodes.FilterableStream, transition_type: transition_type, duration: float, v0_d: float, v1_d: float, a0_d: float, a1_d:float)->tuple[ffmpeg.nodes.FilterableStream, ffmpeg.nodes.FilterableStream, float, float]:
+    
+    if transition_type == transition_type.FADE:
+        temp_clip_v = ffmpeg.filter([v0, v1], 'xfade', transition='fade', duration=duration, offset=(v0_d-duration-(v0_d-a0_d)))
+    else: 
+        print("unsupported transition type")
+
+    temp_clip_a = ffmpeg.filter([a0,a1], 'acrossfade', duration=duration)
+
+    # calculate the durations of the new clip
+    temp_clip_v_d = v0_d + v1_d - duration
+    temp_clip_a_d = a0_d + a1_d - duration
+    return (temp_clip_v, temp_clip_a, temp_clip_v_d, temp_clip_a_d)
 
 def transitions(script: tuple[dict,list[scene]]):
+
     pass
