39a59c3198c561c0b847d3e5994b204f9763cbeb
Arturo Amaya <a1amaya@ucsd.edu>
2024-04-22 16:52:21 -0700

    don't forget to probe when we first compose

--- compose.py
+++ compose.py
@@ -5,6 +5,7 @@ def compose_scenes(script: tuple[dict,list[scene]]):
     script_head, script_body = script
     for scene in script_body:
         scene.avatar_video.video = ffmpeg.input(scene.avatar_video.filename)
+        scene.avatar_video.metadata = ffmpeg.probe(scene.avatar_video.filename)
         if scene.style.style == style_type.PIP:
             scene.clip = scene.avatar_video.video # for now there's not much to do with a pip since we've generated it in heygen
         else:
