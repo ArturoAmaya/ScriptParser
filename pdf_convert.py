0fe8dcdc62c8e95e4ed05f2385682be7f682c7cc
Arturo Amaya <a1amaya@ucsd.edu>
2024-07-05 12:07:34 -0700

    new default pdf

--- pdf_convert.py
+++ pdf_convert.py
@@ -1,7 +1,7 @@
 from pdf2image import convert_from_path
 import PIL.Image
 PIL.Image.MAX_IMAGE_PIXELS = 100005000
-pages = convert_from_path('./long_scripts/W5_Lec9_memorycache.pdf', 400)
+pages = convert_from_path('./long_scripts/W5_Lec9_memorycache.pdf', 48)
 
 for count, page in enumerate(pages):
     page.save(f'./long_scripts/slides/out{count}.jpg', 'JPEG')
