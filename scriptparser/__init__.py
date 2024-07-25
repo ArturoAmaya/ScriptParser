1745f8902758a7bf5475dab4e18b55e310ff7486
Arturo Amaya <a1amaya@ucsd.edu>
2024-07-25 15:05:50 -0700

    export side

--- scriptparser/__init__.py
+++ scriptparser/__init__.py
@@ -4,4 +4,4 @@ from .types.caption import caption
 from .types.scene import scene
 from .types.slide import slide, slide_source
 from .types.transition_in import transition_in, transition_type
-from .types.style import style, style_type
+from .types.style import style, style_type, side
